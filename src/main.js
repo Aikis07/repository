1cf860408482a28dfc4e5adbf490708c2e2cea37
unknown <danila.barkov@gmail.com>
2023-02-17 09:45:44 +0300

    new commit

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import index from '@/assets/css/index.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  index,
   router,
   store,
   render: h => h(App)
